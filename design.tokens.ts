6d6d883536c7066bac698d3a0f62173f345688e3
Paul Shryock <paul@pshry.com>
2022-07-05 16:20:03 -0400

    Install tooling
    - Eleventy
    - Sass
    - PostCSS
    - TypeScript
    - Esbuild
    - Jest
    - Editorconfig
    - Prettier
    - ESLint
    - Stylelint
    - Sort package.json
    - Release Bump
    - Husky
    - Netlify

---
+++ design.tokens.ts
@@ -0,0 +1,13 @@
+interface DesignTokens {
+	[key: string]: string
+}
+
+const designTokens: DesignTokens = {
+	blue: 'blue',
+	darkblue: 'darkblue',
+	navy: 'navy',
+	gray: 'gray',
+	black: 'black',
+}
+
+export default designTokens
---
+++ eleventy.config.cjs
@@ -0,0 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { minify } = require('html-minifier')
+
+module.exports = function(eleventyConfig) {
+	// Minify HTML.
+	eleventyConfig.addTransform('html-minifier', function(content, outputPath) {
+		if (!outputPath || !outputPath.endsWith('.html')) return content
+
+		return minify(content, {
+			useShortDoctype: true,
+			removeComments: true,
+			collapseWhitespace: true,
+		})
+	})
+
+	eleventyConfig.addWatchTarget('./src/fonts/')
+	eleventyConfig.addWatchTarget('./src/img/')
+	eleventyConfig.addWatchTarget('./src/scss/')
+	eleventyConfig.addWatchTarget('./src/ts/')
+
+	return {
+		dir: {
+			data: 'data',
+			includes: 'liquid/components',
+			input: 'src',
+			layouts: 'liquid/layouts',
+			output: 'dist',
+		},
+	}
+}
