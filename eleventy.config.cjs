4c14c392108d1f90c21eb84b5b20199e2793fe79
Paul Shryock <paul@pshry.com>
2022-07-09 03:40:56 -0400

    Add acceptance tests
    - health check
    - performance
    - seo

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,17 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { minify } = require('html-minifier')
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 	// Minify HTML.
 	eleventyConfig.addTransform('html-minifier', function(content, outputPath) {
 		if (!outputPath || !outputPath.endsWith('.html')) return content
 
-    return minify(content, {
-      useShortDoctype: true,
-      removeComments: true,
-      collapseWhitespace: true,
-    })
-  })
+		return minify(content, {
+			useShortDoctype: true,
+			removeComments: true,
+			collapseWhitespace: true,
+		})
+	})
 
 	eleventyConfig.addWatchTarget('./src/fonts/')
 	eleventyConfig.addWatchTarget('./src/img/')
