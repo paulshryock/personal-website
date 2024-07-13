356bf029ddfdb5dfce2697ea41624a37e85579ee
Paul Shryock <paul@pshry.com>
2024-07-13 15:03:10 -0400

    Return request information from /api/hello

--- src/routes/api/hello.ts
+++ src/routes/api/hello.ts
@@ -1,16 +1,21 @@
 import type { Context } from '@netlify/functions'
 
 /**
- * Says hello.
+ * Returns request information.
  *
- * @param  {Request}  request HTTP request.
- * @param  {Context}  context Netlify function context.
- * @return {Response}         HTTP response.
+ * @param  {Request}  request  HTTP request.
+ * @param  {Context}  _context Netlify function context.
+ * @return {Response} HTTP response.
  * @since  unreleased
  */
-export default function hello(request: Request, context: Context): Response {
-	return new Response(JSON.stringify({ context, request }), {
-		status: 200,
-		statusText: 'OK',
-	})
+export default function hello(request: Request, _context: Context): Response {
+	return new Response(
+		JSON.stringify({
+			body: request.body,
+			headers: request.headers,
+			method: request.method,
+			request,
+		}),
+		{ status: 200, statusText: 'OK' },
+	)
 }
