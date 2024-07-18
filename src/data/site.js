01e99a9064ddb310d39e67f2f340f432c8e0ac14
Paul Shryock <paul@pshry.com>
2024-07-18 11:31:44 -0400

    Add site.domain, fix site.host

--- src/data/site.js
+++ src/data/site.js
@@ -1,5 +1,7 @@
 /* istanbul ignore next */
-const host = 'paulshryock.dev'
+const domain = 'paulshryock.dev'
+/* istanbul ignore next */
+const host = `www.${domain}`
 /* istanbul ignore next */
 const title = 'Paul Shryock'
 
@@ -9,6 +11,7 @@ const title = 'Paul Shryock'
  * @type  {{
  *   copyright: string
  *   description: string
+ *   domain: string
  *   host: string
  *   language: string
  *   origin: string
@@ -22,6 +25,7 @@ export default {
 	copyright: `Copyright ${new Date().toLocaleString('en-US', { year: 'numeric' })} ${title}. All rights reserved.`,
 	description:
 		'Senior software engineer with 15+ years experience, currently building better editorial content publishing tools for the NBA.',
+	domain,
 	host,
 	language: 'en-US',
 	origin: `https://${host}`,
